a31edb4206b363b5ceda78e5376d67ed03de8a9d
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-06-19 14:39:13 +0200

    [FEAT] form sends the password and the page converts the form input in an integer value

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
+$pswd_length = 0;
 
+if (!empty($_GET['pswd_length'])) {
+    $pswd_length = intval($_GET['pswd_length']);
+}
+
+if ($pswd_length > 0) {
+    echo "genero password di lunghezza: $pswd_length";
+}
 
 ?>
 
@@ -13,7 +21,10 @@
 </head>
 
 <body>
-    ciao
+    <form action="." method="GET">
+        <input type="text" name="pswd_length" placeholder="Quanti caratteri vuoi inserire?">
+        <button>Genera Password</button>
+    </form>
 </body>
 
 </html>
